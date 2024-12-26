39336402b60ec3b4a63985951401f2ed2e0603de
Guy R <Guy.Roban@Gmail.Com>
2024-12-26 02:53:15 +0200

    add project files
    add project files

--- src/main/java/lcguy/base.java
+++ src/main/java/lcguy/base.java
@@ -11,9 +11,6 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.devtools.DevTools;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -21,10 +18,8 @@ import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
